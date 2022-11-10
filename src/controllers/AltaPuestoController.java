89ac2c8cebf6cef985e6b7640abffed3f74173a9
LucasPairetti <ljpairetti@gmail.com>
2022-11-10 13:01:33 -0300

    agregue mensajes de error y checkeos altaPuesto

--- src/controllers/AltaPuestoController.java
+++ src/controllers/AltaPuestoController.java
@@ -24,6 +24,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class AltaPuestoController implements Initializable {
@@ -72,6 +73,18 @@ public class AltaPuestoController implements Initializable {
     @FXML
     private TextField puestoTextField;
     
+    @FXML
+    private Text ponderacionErrorText;
+    
+    @FXML
+    private Text nombreErrorText;
+    
+    @FXML
+    private Text empresaErrorText;
+    
+    @FXML
+    private Text codigoErrorText;
+    
     private List<ItemCompetencia> listadoDeCompetencias = new ArrayList<ItemCompetencia>();
     
     
@@ -79,6 +92,15 @@ public class AltaPuestoController implements Initializable {
     ObservableList<Competencia> listaCompetencias= FXCollections.observableArrayList(); 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+    	
+    	//escondo los mensajes de error
+    	nombreErrorText.setOpacity(0);
+    	codigoErrorText.setOpacity(0);
+    	empresaErrorText.setOpacity(0);
+    	ponderacionErrorText.setOpacity(0);
+    	
+    	
+    	
         // TODO Auto-generated method stub
         //buscar todas las competencias y agregarlas a listaCompetencias
         listaCompetencias.addAll(gestorCompetencias.getAllCompetencia());
@@ -116,12 +138,12 @@ public class AltaPuestoController implements Initializable {
     @FXML
     void AceptarButtonClicked(ActionEvent event) {
 
-        if(!(codigoTextField.getText()== null || puestoTextField.getText() == null || descripcionTextArea.getText() ==null) ) {
+        if(checkearCamposPuesto()) {
 
             if(gestorPuesto.getPuestoByNombre(puestoTextField.getText())==null && gestorPuesto.getPuestoByCodigo(Integer.parseInt(codigoTextField.getText()))==null) {
                Puesto puesto = gestorPuesto.createPuesto(Integer.parseInt(codigoTextField.getText()), puestoTextField.getText(), empresaTextField.getText(), descripcionTextArea.getText(), listadoDeCompetencias);
                gestorPuesto.createPuesto(puesto);}
-            else System.out.println("Ya existe un puesto con ese codigo o con ese nombre.");
+            else System.out.println("Ya existe un puesto con ese codigo o con ese nombre.");//falta mostrar cuadro alerta en lugar de esto
 
         Parent root;
         try {
@@ -138,12 +160,13 @@ public class AltaPuestoController implements Initializable {
     
     @FXML
     void agregarItemButtonClicked(ActionEvent event) {
+    	if(checkearCamposCompetencia()) {
     	Competencia competenciaElegida = competenciasTableView.getSelectionModel().getSelectedItem();
     	ItemCompetencia nuevoItemCompetencia = new ItemCompetencia (competenciaElegida, Integer.parseInt(ponderacionTextField.getText()));
     	listadoDeCompetencias.add(nuevoItemCompetencia);
     	listaItemCompetencia.add(nuevoItemCompetencia);
     	itemCompetenciatableView.setItems(listaItemCompetencia);
-    	
+    	}
     	
 
     }
@@ -172,6 +195,64 @@ public class AltaPuestoController implements Initializable {
         listaItemCompetencia.remove(itemCompetencia);
         itemCompetenciatableView.setItems(listaItemCompetencia);
     }
+    
+    public Boolean checkearCamposPuesto() { //validos =true, incompletos o con error setea los textos y retorna false
+    	Boolean flag = true;
+    	
+    	
+    	if(codigoTextField.getText()==null) {
+    		flag = false;
+    		codigoErrorText.setText("complete este campo obligatorio");
+    		codigoErrorText.setOpacity(1);
+    	}else codigoErrorText.setOpacity(0);
+    		
+    		
+    	if(empresaTextField.getText()==null) {
+    		flag = false;
+    		empresaErrorText.setText("complete este campo obligatorio");
+    		empresaErrorText.setOpacity(1);
+    	}else empresaErrorText.setOpacity(0);
+    	
+    	if(puestoTextField.getText()==null) {
+    		flag = false;
+    		nombreErrorText.setText("complete este campo obligatorio");
+    		nombreErrorText.setOpacity(1);
+    	}else nombreErrorText.setOpacity(0);
+    	
+    	try {
+    		Integer.parseInt(codigoTextField.getText());
+    		
+    	}catch(Exception e) {
+    		codigoErrorText.setText("caracteres invalidos, solo se aceptan numericos");
+    		codigoErrorText.setOpacity(1);
+    		return false;
+    	}
+    	
+    	
+    	return flag;
+    	
+    	
+    }
+    
+    public Boolean checkearCamposCompetencia() {
+    	Boolean flag= true;
+    	
+    	
+    	if(competenciasTableView.getSelectionModel().isEmpty()) {
+    		ponderacionErrorText.setText("Seleccione una competencia");
+    		ponderacionErrorText.setOpacity(1);
+    	}else  ponderacionErrorText.setOpacity(0);
+    	
+    	if(ponderacionTextField.getText()==null && flag) {
+    		flag = false;
+    		ponderacionErrorText.setText("caracteres invalidos, solo se aceptan numericos");
+    		ponderacionErrorText.setOpacity(1);
+    	} else ponderacionErrorText.setOpacity(0);
+    	
+    	
+		return flag;
+    }
+    
 	
 
 }

9253c3bc78600012e71bf7f6461b2260dd733367
LucasPairetti <ljpairetti@gmail.com>
2022-11-29 11:09:22 -0300

    agregue manejo de errores y arranqué para autenticar

--- src/controllers/IngresoCandidatoController.java
+++ src/controllers/IngresoCandidatoController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class IngresoCandidatoController implements Initializable {
@@ -48,19 +49,66 @@ public class IngresoCandidatoController implements Initializable {
     @FXML
     private Button volverButton;
     
+    @FXML
+    private Text errorClaveText;
+
+    @FXML
+    private Text errorDocText;
+
+    @FXML
+    private Text errorNumText;
+    
     @Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
     	tipoDocComboBox.setItems(FXCollections.observableArrayList("DNI","Pasaporte"));
+    	errorDocText.setOpacity(0);
+    	errorNumText.setOpacity(0);
+    	errorClaveText.setOpacity(0);
 	}
 
     @FXML
     void MostrarContraseña(ActionEvent event) {
 
+    	if(mostrarContraseñaCheck.isSelected()) {
+    		passwordFieldShowed.setText(passwordFieldHidden.getText());
+    		passwordFieldHidden.setVisible(false);
+    		passwordFieldShowed.setVisible(true);
+    		
+    	}else {
+    		passwordFieldHidden.setText(passwordFieldShowed.getText());
+    		passwordFieldShowed.setVisible(false);
+    		passwordFieldHidden.setVisible(true);
+    	
+    	}
     }
 
     @FXML
     void ingresarButtonClicked(ActionEvent event) {
+    	if(!checkearCampos()) {
+    		return;
+    	}
+    	String tipoDoc= tipoDocComboBox.getSelectionModel().getSelectedItem();
+    	int  nroDoc=Integer.parseInt(usuarioTextField.getText());
+    	
+    	if(passwordFieldHidden.getText().length()>=passwordFieldShowed.getText().length()) { //este if lo pongo xq como son dos textfield, no se cual es el completo (asumo que es el que tiene mas caracteres)
+    		int clave=Integer.parseInt(passwordFieldHidden.getText()); 
+    	}else {
+    		int clave=Integer.parseInt(passwordFieldShowed.getText()); 
+    	}
+    	
+    	
+    	
+    	
+    	
+    	
+    	/*
+    	 * tenes tipoDoc, nroDoc y clave para como atributos para hacer la validacion
+    	 * misma logica que en validar consultor, solo que aun no tengo pensada la interfaz que le sigue a esto
+    	 * 
+    	 */
+    	
+    	
 
     }
 
@@ -86,7 +134,65 @@ public class IngresoCandidatoController implements Initializable {
 
 	
     
-    
+    Boolean checkearCampos() {
+    	Boolean flag=true;
+    	
+    	if(tipoDocComboBox.getSelectionModel().getSelectedItem()==null) {
+    		flag=false;
+    		errorDocText.setText("elija un tipo de documento");
+    		errorDocText.setOpacity(1);
+    		
+    	}else errorDocText.setOpacity(0);
+    	if(usuarioTextField.getText()=="") {
+    		 flag=false;
+    		 errorNumText.setText("Complete este campo obligatorio");
+    		 errorNumText.setOpacity(1);
+    	}else errorNumText.setOpacity(0);
+    	if ((passwordFieldShowed.getText()==""&&passwordFieldHidden.getText()=="")) {
+    		flag=false;
+    		errorClaveText.setText("Complete este campo obligatorio");
+    		errorClaveText.setOpacity(1);
+    	 }else errorClaveText.setOpacity(0);
+    	
+    	
+    	try {
+    		if(flag) {
+    		Integer.parseInt(usuarioTextField.getText());
+    		}
+    		
+
+    		
+    	}catch(Exception e) {
+    		
+    		errorNumText.setText("Caracteres invalidos, solo se aceptan numericos");
+    		
+    		errorNumText.setOpacity(1);
+    		return false;
+    	}
+    	
+    	try {
+    		if(flag &&passwordFieldHidden.getText()!="" ) {
+    		Integer.parseInt(passwordFieldHidden.getText());
+    		}else if(flag &&passwordFieldShowed.getText()!="" ) {
+        		Integer.parseInt(passwordFieldShowed.getText());
+        		}
+    		
+
+    		
+    	}catch(Exception e) {
+    		
+    		errorClaveText.setText("Caracteres invalidos, solo se aceptan numericos");
+    		
+    		errorClaveText.setOpacity(1);
+    		return false;
+    	}
+    	
+    	
+    	
+    	
+    	
+    	return flag;
+    }
     
     
     
--- src/controllers/IngresoConsultorController.java
+++ src/controllers/IngresoConsultorController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class IngresoConsultorController implements Initializable {
@@ -43,10 +44,17 @@ public class IngresoConsultorController implements Initializable {
     @FXML
     private Button volverButton;
     
+    @FXML
+    private Text errorPasswordText;
+
+    @FXML
+    private Text errorUsuarioText;
+    
     @Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
+	public void initialize(URL arg0, ResourceBundle arg1) { //este metodo que siempre está, es para inicializar la pantalla con datos o parametros
 		// TODO Auto-generated method stub
-		
+    	errorUsuarioText.setOpacity(0);
+    	errorPasswordText.setOpacity(0);
 	}
   
 
@@ -54,10 +62,26 @@ public class IngresoConsultorController implements Initializable {
     void ingresarButtonClicked(ActionEvent event) {
     	
     	//cuando se haga la validacion, revisar que se se tomen los valores de 
+    	if(!validarCampos()) {
+    		return;
+    	}
+    	String usuario= usuarioTextField.getText();
+    	if(passwordFieldHidden.getText().length()>=passwordFieldShowed.getText().length()) {//este if esta xq no se si un campo esta mas completo que otro, tomo como contraseña el que tiene mayor longitud
+    		String password= passwordFieldHidden.getText();
+    	}else {
+    		String password= passwordFieldShowed.getText();
+    	}
     	
     	
+    	/*
+    	 * ahora tenes usuario y password como atributos para validar
+    	 * aca tendria que ir la logica de validar al usuario, es mas facil decir "si no se cumplen los parametros -> return;" cosa de no necesitar meter lo de interfaz
+    	 * dentro de un if o algo
+    	 * 
+    	 */
+    
     	
-    	
+    	//de aca para abajo, es enviar al usuario a la siguiente pantalla, asi que esto debe ocurrir solo si se validó
     	Parent root;
 		try {
 			root = FXMLLoader.load((getClass().getResource("/views/UsuarioConsultor.fxml")));
@@ -106,5 +130,24 @@ public class IngresoConsultorController implements Initializable {
 		}
     }
 	
+     boolean validarCampos() { //esto revisa que los campos no esten incompletos
+    	 Boolean flag = true;
+    	 
+    	 if(usuarioTextField.getText()=="") {
+    		 flag=false;
+    		 errorUsuarioText.setText("Complete este campo obligatorio");
+    		 errorUsuarioText.setOpacity(1);
+    		 
+    	 }else errorUsuarioText.setOpacity(0);
+    		 
+    	if ((passwordFieldShowed.getText()==""&&passwordFieldHidden.getText()=="")) {
+    		flag=false;
+    		errorPasswordText.setText("Complete este campo obligatorio");
+    		errorPasswordText.setOpacity(1);
+    	 }else errorPasswordText.setOpacity(0);
+    		
+    	 return flag;
+    	 
+     }
 
 }

5875aa0e390cd703fd1a2b2a6ed404c2b79f83f4
LucasPairetti <ljpairetti@gmail.com>
2022-12-12 12:18:09 -0300

    alertas y evaluacion actualizadas

--- src/controllers/EvaluarBusquedaController.java
+++ src/controllers/EvaluarBusquedaController.java
@@ -1,12 +1,19 @@
 package controllers;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
+
+import DTOS.CompetenciaDTO;
 import entidades.Candidato;
 import gestores.GestorDeCandidato;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -16,21 +23,22 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class EvaluarBusquedaController {
+public class EvaluarBusquedaController implements Initializable {
 	
 	//public List<Candidato> listaCandidatos;
+	GestorDeCandidato gestorCandidatos = GestorDeCandidato.getInstance();
 	
     @FXML
-    private TableView<?> CandidatoTableView;
+    private TableView<Candidato> CandidatoTableView;
 
     @FXML
     private Button agregarButton;
 
     @FXML
-    private TableColumn<?, ?> apellidoBuscarColumn;
+    private TableColumn<Candidato, String> apellidoBuscarColumn;
 
     @FXML
-    private TableColumn<?, ?> apellidoColumn;
+    private TableColumn<Candidato, String> apellidoColumn;
 
     @FXML
     private TextField apellidoTextField;
@@ -39,19 +47,21 @@ public class EvaluarBusquedaController {
     private Button buscarButton;
 
     @FXML
-    private TableView<?> buscarCandidatoTableView;
+    private TableView<Candidato> buscarCandidatoTableView;
 
     @FXML
-    private TableColumn<?, ?> nombreBuscarColumn;
+    private TableColumn<Candidato, String> nombreBuscarColumn;
 
     @FXML
-    private TableColumn<?, ?> nombreColumn;
+    private TableColumn<Candidato, String> nombreColumn;
 
     @FXML
     private TextField nombreTextField;
 
     @FXML
-    private TableColumn<?, ?> nroBuscarColumn;
+    private TableColumn<Candidato, Integer> nroBuscarColumn;
+    @FXML
+    private TableColumn<Candidato, Integer> nroCandidatoColumn;
 
     @FXML
     private TextField nroTextField;
@@ -67,32 +77,63 @@ public class EvaluarBusquedaController {
 
     @FXML
     private Button siguienteButton;
+    
+    ObservableList<Candidato> listaCandidatosBuscados = FXCollections.observableArrayList(); 
+    ObservableList<Candidato> listaCandidatosSeleccionados = FXCollections.observableArrayList(); 
+    
+    
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+    	// TODO Auto-generated method stub
+    	listaCandidatosBuscados.addAll(gestorCandidatos.getAllCandidato());
+    	buscarCandidatoTableView.setItems(listaCandidatosBuscados);
+    	
+    }
+    
+    
+    
 
     @FXML
     void agregarButtonClicked(ActionEvent event) {
+    	listaCandidatosSeleccionados.add(buscarCandidatoTableView.getSelectionModel().getSelectedItem());
+    	CandidatoTableView.setItems(listaCandidatosBuscados);
     	
     }
 
     @FXML
     void buscarButtonClicked(ActionEvent event) {
-    	GestorDeCandidato gestorCandidatos = GestorDeCandidato.getInstance();
+    	
     	int nroCandidato;
     	if(nroTextField.equals("")) nroCandidato = -1; else nroCandidato = Integer.parseInt(nroTextField.getText());
     	List<Candidato> candidatosEncontrados = 
     			gestorCandidatos.buscarCandidatos(apellidoTextField.getText(), 
     			nombreTextField.getText(), 
     			nroCandidato);
+    	buscarCandidatoTableView.setItems((ObservableList<Candidato>) candidatosEncontrados);
     	
     }
 
     @FXML
     void quitarButtonClicked(ActionEvent event) {
+    	if(CandidatoTableView.getSelectionModel().getSelectedItem()!=null) {
+    		listaCandidatosSeleccionados.remove(CandidatoTableView.getSelectionModel().getSelectedItem());
+    		CandidatoTableView.setItems(listaCandidatosSeleccionados);
+    	}
     	
     }
 
     @FXML
     void salirClicked(ActionEvent event) {
-
+    	Parent root;
+		try {
+			root = FXMLLoader.load((getClass().getResource("/views/UsuarioConsultor.fxml")));
+			Stage window = (Stage)salirButton.getScene().getWindow();
+			window.setTitle("Usuario consultor");
+	    	window.setScene(new Scene(root));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     @FXML
@@ -102,16 +143,26 @@ public class EvaluarBusquedaController {
     	GestorDeCandidato gestorCandidato = GestorDeCandidato.getInstance();
     	if(gestorCandidato.validarCuestionarios(listaCandidatos)) llamo interfaz;
     	//else IncluirAlerta()*/
-    	EvaluarFuncionInterfaz(null);
+    	EvaluarFuncionInterfaz(listaCandidatosSeleccionados);
 
     }
     
     
     void alertaUsuarioEnCurso(List<Candidato> listaEnCurso) {
+    	
+    	
+    	FXMLLoader loader = new FXMLLoader();
+    	loader.setLocation(getClass().getResource("/views/alertaUsuariosEnCurso.fxml"));
     
     	Stage alertaStage = new Stage();
     	Parent root;
 		try {
+			loader.load();
+			
+			AlertaUsuariosEnCursoController display = loader.getController();
+			display.setCandidatosEnCurso(listaEnCurso);
+			
+			
 			root = FXMLLoader.load((getClass().getResource("/views/alertaUsuariosEnCurso.fxml")));
 			Scene scene = new Scene(root);
 			alertaStage.setTitle("Candidatos en curso");
@@ -143,5 +194,7 @@ public class EvaluarBusquedaController {
 	   
 	   
    }
+
+
 
 }
